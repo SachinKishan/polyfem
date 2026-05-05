1e41ca0300ba387b536222bae7f4340f593d6714
Sachin Kishan <33657481+SachinKishan@users.noreply.github.com>
2026-05-04 22:55:02 -0400

    amips square

--- src/polyfem/assembler/AMIPSEnergy.hpp
+++ src/polyfem/assembler/AMIPSEnergy.hpp
@@ -66,7 +66,8 @@ namespace polyfem::assembler
 
 			const T powJ = pow(det, power);
 			const double weight = get_energy_weight(el_id);
-			return T(weight) * (def_grad.transpose() * def_grad).trace() / powJ; //+ barrier<T>::value(det);
+			auto value = T(weight) * (def_grad.transpose() * def_grad).trace() / powJ;
+			return value*value; //+ barrier<T>::value(det);
 		}
 
 	private:
